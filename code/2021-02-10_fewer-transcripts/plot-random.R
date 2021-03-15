b59cf7a14cd3046b4c2f918ecb9575c481a06696
James Hawley <jamesrichardhawley@gmail.com>
2021-03-15 16:33:17 -0400

    Updated figures with randomly selected transcripts

--- code/2021-02-10_fewer-transcripts/plot-random.R
+++ code/2021-02-10_fewer-transcripts/plot-random.R
@@ -26,7 +26,7 @@ PLOT_DIR <- file.path(RESULT_DIR, "Plots")
 loginfo("Loading data")
 
 # load comparison data
-comp <- fread(file.path(RESULT_DIR, "Comparison", "random", "err.tsv"))
+comp <- fread(file.path(RESULT_DIR, "Comparison", "random", "simulations.tsv"))
 
 # depending on which genes were filtered, not all 3 methods may have the same transcripts
 comp <- comp[complete.cases(comp)]
@@ -35,40 +35,39 @@ comp <- comp[complete.cases(comp)]
 # ==============================================================================
 # Analysis
 # ==============================================================================
-# convert to wide format for plotting
-comp_wide_mean <- dcast(
-    # replace spaces in Method column with underscores on the fly for easier column access after casting
-    comp[, .(
-        target_id,
-        Total,
-        Method = gsub(" ", "_", Method),
-        Mean_SE
-    )],
-    target_id + Total ~ Method,
-    value.var = "Mean_SE"
-)
-comp_wide_sd <- dcast(
-    # replace spaces in Method column with underscores on the fly for easier column access after casting
-    comp[, .(
-        target_id,
-        Total,
-        Method = gsub(" ", "_", Method),
-        SD_SE
-    )],
-    target_id + Total ~ Method,
-    value.var = "SD_SE"
-)
+# # convert to wide format for plotting
+# comp_wide_mean <- dcast(
+#	 # replace spaces in Method column with underscores on the fly for easier column access after casting
+#	 comp[, .(
+#		 target_id,
+#		 Total,
+#		 Method = gsub(" ", "_", Method),
+#		 Mean_SE
+#	 )],
+#	 target_id + Total ~ Method,
+#	 value.var = "Mean_SE"
+# )
+# 
+# comp_wide_sd <- dcast(
+#	 # replace spaces in Method column with underscores on the fly for easier column access after casting
+#	 comp[, .(
+#		 target_id,
+#		 Total,
+#		 Method = gsub(" ", "_", Method),
+#		 SD_SE
+#	 )],
+#	 target_id + Total ~ Method,
+#	 value.var = "SD_SE"
+# )
+# 
+# # merge Mean and SD calculations into the same table
+# comp_wide <- merge(
+#	 x = comp_wide_mean,
+#	 y = comp_wide_sd,
+#	 suffixes = c("_mean", "_sd")
+# )
+# comp_wide <- comp_wide[complete.cases(comp_wide)]
 
-# merge Mean and SD calculations into the same table
-comp_wide <- merge(
-    x = comp_wide_mean,
-    y = comp_wide_sd,
-    suffixes = c("_mean", "_sd")
-)
-comp_wide <- comp_wide[complete.cases(comp_wide)]
-
-# calculate relevant statistics
-err <- 
 
 # ==============================================================================
 # Plots
@@ -76,68 +75,50 @@ err <-
 loginfo("Plotting figures")
 # ensure that the directory exists
 if (!dir.exists(PLOT_DIR)) {
-    dir.create(PLOT_DIR, recursive = TRUE)
+	dir.create(PLOT_DIR, recursive = TRUE)
 }
 
 gg <- (
-    ggplot(data = comp_wide_mean)
-    + geom_point(aes(x = Unbalanced_OLS, y = Unbalanced_JS))
-    + geom_abline(slope = 1, intercept = 0, linetype = "dashed")
-    + facet_wrap(~ Total)
-    + theme_minimal()
+	ggplot(data = comp)
+	+ geom_point(aes(x = Unbalanced_OLS, y = Unbalanced_JS))
+	+ geom_abline(slope = 1, intercept = 0, linetype = "dashed")
+	+ facet_wrap(~ Total)
+	+ theme_minimal()
 )
 ggsave(
-    file.path(PLOT_DIR, "random.unbalanced-comparison.png"),
-    gg,
-    width = 12,
-    height = 8,
-    units = "cm"
+	file.path(PLOT_DIR, "random.unbalanced-comparison.png"),
+	gg,
+	width = 12,
+	height = 8,
+	units = "cm"
 )
 
 gg <- (
-    ggplot(data = comp)
-    + geom_col(aes(x = Method, y = Mean_SE, fill = Method))
-    + scale_x_discrete(
-        name = NULL
-    )
-    + scale_y_continuous(
-        name = "Fold-Change MSE"
-    )
-    + facet_wrap(~ Total, scales = "free_y")
-    + guides(fill = FALSE)
-    + theme_minimal()
-    + theme(
-        axis.text.x = element_text(colour = "#000000", angle = 90, hjust = 1, vjust = 0.5)
-    )
+	ggplot(data = comp)
+	+ geom_col(
+		aes(
+			x = target_id,
+			y = 100 * (Unbalanced_JS - Unbalanced_OLS) / Unbalanced_OLS
+		)
+	)
+	+ scale_x_discrete(
+		name = NULL
+	)
+	+ scale_y_continuous(
+		name = "MSE % Difference",
+	)
+	+ guides(fill = FALSE)
+	+ theme_minimal()
+	+ theme(
+		axis.text.x = element_text(colour = "#000000", angle = 90, hjust = 1, vjust = 0.5),
+		panel.grid.major.x = element_blank()
+	)
 )
 ggsave(
-    file.path(PLOT_DIR, "random.mse.png"),
-    gg,
-    width = 16,
-    height = 10,
-    units = "cm"
+	file.path(PLOT_DIR, "random.mse.percent-change.png"),
+	gg,
+	width = 16,
+	height = 10,
+	units = "cm"
 )
 
-gg <- (
-    ggplot(data = comp_wide_mean)
-    + geom_col(aes(x = gene_name, y = 100 * (Unbalanced_JS - Unbalanced_OLS) / Unbalanced_OLS))
-    + scale_x_discrete(
-        name = NULL
-    )
-    + scale_y_continuous(
-        name = "MSE % Difference",
-    )
-    + guides(fill = FALSE)
-    + theme_minimal()
-    + theme(
-        axis.text.x = element_text(colour = "#000000", angle = 90, hjust = 1, vjust = 0.5),
-        panel.grid.major.x = element_blank()
-    )
-)
-ggsave(
-    file.path(PLOT_DIR, "mse.percent-change.png"),
-    gg,
-    width = 16,
-    height = 10,
-    units = "cm"
-)
